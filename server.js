58a6f6b1ce0ce6ec44dca66fef713fdbc5673bed
Aadityasiva <67770098+aadityasivaS@users.noreply.github.com>
2020-11-19 10:55:51 +0530

    Done

--- server.js
+++ server.js
@@ -8,26 +8,10 @@
 const express = require("express");
 const app = express();
 
-const prompt = require("prompt-sync")();
+app.get("/", (_req, res) => {
+  res.send("It works! The boilerplate works! Yay!");
+});
 
-const port = prompt("Which port? ");
-
-if (isNaN(port)) {
-  //port should be a number
-  console.log("error :");
-  console.error("Port should be a number.");
-} else if (port == "1" || port == 1) {
-  //port 1 cant be used
-  console.log("error :");
-  console.error("Give a port other than one.");
-} else {
-  // if it gets past all guards, send the response
-
-  app.get("/", (_req, res) => {
-    res.send("It works! The boilerplate works! Yay!");
-  });
-
-  app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-  });
-}
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
